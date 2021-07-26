d841a75395f367fdaba3e054fa4826ffcf0afa3e
elfy-f <84900275+elfy-f@users.noreply.github.com>
2021-07-26 13:51:36 +0200

    depart

---
+++ src/Controller/MainController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController
+{
+   /**
+    * @Route("/", name="main_home")
+    */
+    public function home()
+    {
+
+
+
+    }
+
+    /**
+     * @Route("/profil", name="main_profil")
+     */
+    public function profil()
+    {
+
+
+
+    }
+
+
+}
