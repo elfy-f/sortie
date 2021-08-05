d5b2d3ed732c003b4cc41316d45d152e455dac4b
elfy-f <84900275+elfy-f@users.noreply.github.com>
2021-08-05 14:56:41 +0200

    fleur

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+/*
+ * @Route("/admin", name="admin_")
+ */
+
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Require ROLE_ADMIN for *every* controller method in this class.
+ *
+ * @IsGranted("ROLE_ADMIN")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin_")
+     * @Route("/dashboard", name="dashboard")
      */
     public function index(): Response
     {
